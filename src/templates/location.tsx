16bf9ac8a8e4a74dc45547ff11c54edbe53ebbe1
rkeerthi <sunnykeerthi@gmail.com>
2022-09-27 02:45:29 +0530

    changed nav path

--- src/templates/location.tsx
+++ src/templates/location.tsx
@@ -82,11 +82,7 @@ export const config: TemplateConfig = {
  * take on the form: featureName/entityId
  */
 export const getPath: GetPath<TemplateProps> = ({ document }) => {
-  return document.slug
-    ? document.slug
-    : `${document.locale}/${document.address.region}/${document.address.city}/${
-        document.address.address1
-      }-${document.id.toString()}`;
+  return document.id.toString();
 };
 
 /**
